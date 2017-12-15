a16d9c18e7ed3f69ee124abb3ff32344a49a9c06
RAMASY Heritier . A <heritier.ramasy@gmail.com>
2017-12-15 11:01:41 +0300

    addPage

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+    @titre = "home"
+  end
+
+  def contact
+    @titre = "contact"
+  end
+  
+  def about    
+    @titre = "about"
+  end
+end
