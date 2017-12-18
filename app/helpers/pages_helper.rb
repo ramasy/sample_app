80f3a07e50be6c299c0ab03e67d747db49878541
RAMASY Heritier . A <heritier.ramasy@gmail.com>
2017-12-18 11:49:04 +0300

    add signup page and named routes

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,5 @@
 module PagesHelper
+	def logo
+		logo = image_tag "logo.png", :alt => "Application Exemple", :class => "round"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'pages/about' => 'pages#about' , as: :app_about
   get 'pages/contact' => 'pages#contact' , as: :app_contact
   get 'pages/help' => 'pages#help' , as: :app_help
+  get 'signup' => 'users#new' , as: :app_signup
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
