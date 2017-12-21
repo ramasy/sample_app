f679ef3c8e04256eb9cd7443ab5341f6133f020d
RAMASY Heritier . A <heritier.ramasy@gmail.com>
2017-12-21 11:13:27 +0300

    add secure option for gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 	def gravatar_for (user, size = 50)
-		gravatar_option = {default: :identicon, size: size}
+		gravatar_option = {default: :identicon, size: size , secure: true}
 		gravatar_image_tag(user.email.downcase, alt: user.nom, title: user.nom ,class: "gravatar", gravatar: gravatar_option) 
 	end
 end
